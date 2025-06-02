3b06942fa64281006eb6a5c8b54d08823fc61c38
Shaashwat Sharma <shaashwatsharma@Shaashwats-Macbook-Pro.local>
2025-06-02 16:26:45 +0530

    small change

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,21 +6,21 @@ export const Navbar=()=>{
     <div className="container flex flex-wrap items-center justify-between mx-auto text-slate-800">
         <a href="#"
         className="mr-4 block cursor-pointer py-1.5 text-base text-slate-800 font-semibold">
-        Material Tailwind
+        PetroSafe
         </a>
         <div className="hidden lg:block">
         <ul className="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
             <li className="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-            <a href="#" className="flex items-center">Pages</a>
+            <a href="#" className="flex items-center">Dashboard</a>
             </li>
             <li className="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-            <a href="#" className="flex items-center">Account</a>
+            <a href="#" className="flex items-center">History</a>
             </li>
             <li className="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-            <a href="#" className="flex items-center">Blocks</a>
+            <a href="#" className="flex items-center">Alerts</a>
             </li>
             <li className="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-            <a href="#" className="flex items-center">Docs</a>
+            <a href="#" className="flex items-center">Contact Us</a>
             </li>
         </ul>
         </div>
